7d5818c8da39fea03f59813598235850ba8d5cad
JarodRankin <jarodr1187@yahoo.ca>
2025-01-09 16:38:25 -0500

    #1-created a new project

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1>The Noun Game</h1>
+      <h2>Also known as the person, place, thing game</h2>
+      <button>Start a Game</button>
+    </div>
+  );
+}
+
+export default App;
